2bd1ffcf3ad3c216ae31f44c408421418354c133
iammadsfq <13523135@std.stei.itb.ac.id>
2026-03-08 09:51:47 +0700

    featL add skeleton for optimizer classes

---
+++ src/models/optimizers.py
@@ -0,0 +1,10 @@
+class SGD:
+    def __init__(self, parameters, lr):
+        self.parameters = parameters
+        self.lr = lr
+
+    def step(self):
+        pass
+
+class Adam: # ini kalo mw implement adam
+    pass
