1fa60a86ed776c383712c8e71c6273a304e99b55
iammadsfq <13523135@std.stei.itb.ac.id>
2026-03-11 03:49:40 +0700

    feat: implement Tensor addition with proper broadcasting

--- src/models/engine.py
+++ src/models/engine.py
@@ -9,11 +9,27 @@ class Tensor:
         self._backward = lambda: None
 
     def __add__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
         out = Tensor(self.data + other.data, (self, other), '+')
 
         def _backward():
-            self.grad += out.grad
-            other.grad += out.grad
+            grad_self = out.grad
+            grad_other = out.grad
+            
+            while grad_self.ndim > self.data.ndim:
+                grad_self = np.sum(grad_self, axis=0)
+            for i in range(self.data.ndim):
+                if self.data.shape[i] == 1 and grad_self.shape[i] > 1:
+                    grad_self = np.sum(grad_self, axis=i, keepdims=True)
+            
+            while grad_other.ndim > other.data.ndim:
+                grad_other = np.sum(grad_other, axis=0)
+            for i in range(other.data.ndim):
+                if other.data.shape[i] == 1 and grad_other.shape[i] > 1:
+                    grad_other = np.sum(grad_other, axis=i, keepdims=True)
+            
+            self.grad += grad_self
+            other.grad += grad_other
         
         out._backward = _backward
         return out
@@ -23,7 +39,7 @@ class Tensor:
 
         def _backward():
             self.grad += out.grad @ other.data.T
-            other.grad *= self.data.T @ out.grad
+            other.grad += self.data.T @ out.grad
         
         out._backward = _backward
         return out
