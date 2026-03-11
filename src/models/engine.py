49711b179d0ca5256a4c5f72a206aa82e132aa50
iammadsfq <13523135@std.stei.itb.ac.id>
2026-03-11 08:45:06 +0700

    feat: implement Tensor scalar multiplication with proper broadcasting

--- src/models/engine.py
+++ src/models/engine.py
@@ -49,9 +49,24 @@ class Tensor:
         out = Tensor(self.data * other.data, (self, other), '*')
 
         def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
-            
+            grad_self = other.data * out.grad
+            grad_other = self.data * out.grad
+
+            while grad_self.ndim > self.data.ndim:
+                grad_self = np.sum(grad_self, axis=0)
+            for i in range (self.data.ndim):
+                if self.data.shape[i] == 1 and grad_self.shape[i] > 1:
+                    grad_self = np.sum(grad_self, axis=i, keepdims=True)
+
+            while grad_other.ndim > other.data.ndim:
+                grad_other = np.sum(grad_other, axis=0)
+            for i in range(other.data.ndim):
+                if other.data.shape[i] == 1 and grad_other.shape[i] > 1:
+                    grad_other = np.sum(grad_other, axis=i, keepdims=True)
+
+            self.grad += grad_self
+            other.grad += grad_other
+
         out._backward = _backward
         return out
 
