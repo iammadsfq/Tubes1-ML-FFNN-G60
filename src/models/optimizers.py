fea791da111e87173210aaa64603ba15edd25ef3
L4mbads <rizacal.mamen@gmail.com>
2026-03-15 11:06:44 +0700

    refactor: optimizer abstractionn

--- src/models/optimizers.py
+++ src/models/optimizers.py
@@ -1,4 +1,12 @@
-class SGD:
+from abc import ABC, abstractmethod
+
+# aku cinta abstraksi
+class BaseOptimizer(ABC):
+     @abstractmethod
+     def step(self) -> None:
+          pass
+
+class SGD(BaseOptimizer):
     def __init__(self, parameters, lr):
         self.parameters = parameters
         self.lr = lr
@@ -7,8 +15,10 @@ class SGD:
         for p in self.parameters:
                 p.data -= self.lr * p.grad
 
-class Adam: # ini kalo mw implement adam
-    # mas adam~ mas adam~
+class Adam(BaseOptimizer): # ini kalo mw implement adam
 
+    # mas adam~ mas adam~
     # btw implement Adam ya, jangan AdamW
-    pass
+
+    def step(self) -> None:
+         pass
